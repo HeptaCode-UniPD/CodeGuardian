88cc2219277abb72f2f8608fc7013d8013eabc7a
angela <angela.canazza@studenti.unipd.it>
2026-03-16 15:48:32 +0100

    cambiato nome ad un file per lo standard

---
+++ frontend/src/utils/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Repositories from '../pages/Repositories';
+import DettagliRepo from '../pages/DettagliRepo';
+
+const router = createBrowserRouter([
+  {
+    path: "/repositories",
+    element: <Repositories />,
+  },
+  {
+    path: "/",
+    element: <Repositories />,
+  },
+  {
+    path: "/repository/:id", 
+    element: <DettagliRepo />,
+  },
+  {
+    path: "*",
+    element: <div>404 - Pagina non trovata</div>,
+  }
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
